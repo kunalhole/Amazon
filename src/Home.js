3d598e2a7941a78161d797a4236c4e415c7a0c48
Kunal Hole <kunalhole538@gmail.com>
2022-04-03 16:38:20 +0530

    Final Commit

--- src/Home.js
+++ src/Home.js
@@ -14,35 +14,35 @@ const Home = () => {
       <div className="home__row">
         <Product
           id="1"
-          title="Onion"
-          price={5}
-          rating={3}
-          image="https://imgs.search.brave.com/7mCfbr4Uq2RTc2ofXtvhgWPbzcvgAJEKRGDYs0vwdQs/rs:fit:713:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5R/ZHRRQkVuTVQyTlRP/RTg2UXl3dktnSGFF/NyZwaWQ9QXBp"
+          title="boAt Storm 1.3 CurvedDisplay Smartwatch"
+          price={2499}
+          rating={5}
+          image="https://imgs.search.brave.com/QiWYzuQlbxBjqytg4SpMvALpQK2k8Cip9Rlm41vXqcI/rs:fit:1097:1200:1/g:ce/aHR0cHM6Ly9jZG4x/LnNtYXJ0cHJpeC5j/b20vcngtaXZTVU1q/ZkVsLXcxMjAwLWgx/MjAwL2JvYXQtc3Rv/cm0tc21hcnR3YXRj/LmpwZw"
         />
 
         <Product
           id="1"
-          title="Tomato without Basket"
-          price={15}
-          rating={3}
-          image="https://imgs.search.brave.com/FtQufKarsYE7UmaYOlgzdRWgoLVwWK_UicOv17YTufE/rs:fit:713:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5f/djVHbHVtQUp1bTNf/QU1kclBNUnd3SGFF/NyZwaWQ9QXBp"
+          title="boAt Airdopes 443 True Wireless Earbuds Best Price in India"
+          price={1500}
+          rating={5}
+          image="https://imgs.search.brave.com/2dH38VFJmwSgKvOrVzcYtsHNkYJJL5PiLNdYPaSD8So/rs:fit:1200:1065:1/g:ce/aHR0cHM6Ly9jZG4x/LnNtYXJ0cHJpeC5j/b20vcngtaUFRM1g3/bTBPLXcxMjAwLWgx/MjAwL2JvYXQtYWly/ZG9wZXMtNDQzLXRy/LmpwZw"
         />
         {/* Product */}
       </div>
       <div className="home__row">
         <Product
           id="1"
-          title="Petorl"
-          price={150}
-          rating={5}
-          image="https://imgs.search.brave.com/7ARPwBMC51ecywRdwN54vRnxHSnVU-kZeaaloy4Y41g/rs:fit:844:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC4y/VHhueWJvdllrOGN0/OC1QR1lJX2lBSGFF/SyZwaWQ9QXBp"
+          title="Psychology Of Money Book"
+          price={999}
+          rating={4}
+          image="https://imgs.search.brave.com/OHcvUJ8JcFTflhyArvpaDg6pGMv-wzsmTyzc4FkvqP8/rs:fit:768:512:1/g:ce/aHR0cHM6Ly9yb2tp/bmd6LmNvbS93cC1j/b250ZW50L3VwbG9h/ZHMvMjAyMS8wNy9U/aGUtUHN5Y2hvbG9n/eS1vZi1Nb25leS03/Njh4NTEyLmpwZw"
         />
         <Product
           id="1"
-          title="Iphone 13 Pro Max"
-          price={150}
+          title="boAt Rockerz 510 Wired And Wireless Headphone"
+          price={1499}
           rating={5}
-          image="https://imgs.search.brave.com/9Prz556sTiZdPuJCrJpi-kztwO6pu4gsLBdGbNa9rzQ/rs:fit:1200:630:1/g:ce/aHR0cHM6Ly9zdG9y/ZS5zdG9yZWltYWdl/cy5jZG4tYXBwbGUu/Y29tLzQ5ODIvYXMt/aW1hZ2VzLmFwcGxl/LmNvbS9pcy9pcGhv/bmUtMTMtcHJvLW1h/eC1ncmFwaGl0ZS1z/ZWxlY3Q_d2lkPTEy/MDAmaGVpPTYzMCZm/bXQ9anBlZyZxbHQ9/OTUmLnY9MTYzMTY1/Mjk1NjAwMA"
+          image="https://imgs.search.brave.com/CMQmq_L7wLRmAyWU_72rJl9aBhLbcEVGhoVE4rX1D6E/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly93d3cu/Y29tcGFyZXByaXgu/aW4vaW1hZ2VzL3By/b2R1Y3QvbGFyZ2Uv/Ym9hdC1yb2NrZXJ6/LTUxMC13aXJlZC1h/bmQtd2lyZWxlc3Mt/aGVhZHBob25lLWxh/cmdlLmpwZWc"
         />
         <Product
           id="1"
@@ -55,10 +55,10 @@ const Home = () => {
       <div className="home__row">
         <Product
           id="1"
-          title="Iphone 13 Pro Max"
-          price={150}
-          rating={5}
-          image="https://imgs.search.brave.com/9Prz556sTiZdPuJCrJpi-kztwO6pu4gsLBdGbNa9rzQ/rs:fit:1200:630:1/g:ce/aHR0cHM6Ly9zdG9y/ZS5zdG9yZWltYWdl/cy5jZG4tYXBwbGUu/Y29tLzQ5ODIvYXMt/aW1hZ2VzLmFwcGxl/LmNvbS9pcy9pcGhv/bmUtMTMtcHJvLW1h/eC1ncmFwaGl0ZS1z/ZWxlY3Q_d2lkPTEy/MDAmaGVpPTYzMCZm/bXQ9anBlZyZxbHQ9/OTUmLnY9MTYzMTY1/Mjk1NjAwMA"
+          title="Sony 50 Inch LED Full HD Smart TV Black KDL 50W660F"
+          price={15000}
+          rating={4}
+          image="https://imgs.search.brave.com/znibmoVXZ_RMG44JO2tpc7o-V8cYkwKzFmLCb9V72J0/rs:fit:1080:1080:1/g:ce/aHR0cHM6Ly9tdWJh/cmFrdGVjaC5jby5r/ZS93cC1jb250ZW50/L3VwbG9hZHMvMjAy/MC8xMC9Tb255LTUw/LUluY2gtTEVELUZ1/bGwtSEQtU21hcnQt/VFYtQmxhY2stS0RM/LTUwVzY2MEYtYS5q/cGc"
         />
       </div>
     </div>
